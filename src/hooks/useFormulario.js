57f3a5a93d9861f3fcbb3c7793127745b17c299d
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-02-25 19:46:38 -0300

    Gestor de Usuarios

---
+++ src/hooks/useFormulario.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+const useFormulario = (inicial) => {
+  const [formulario, setFormulario] = useState(inicial)
+  const handleChange = ({target}) => {
+    setFormulario({
+      ...formulario,
+      [target.name]: target.value
+    })
+  }
+
+  const reset = () => {
+    setFormulario(inicial)
+  }
+
+  return [formulario, handleChange, reset]
+}
+
+export default useFormulario
